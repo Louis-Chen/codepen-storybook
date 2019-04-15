3defd9e41f5aabaabae0228cc61fbf240f0aabe0
LuisChen <leo5916267@gmail.com>
2019-04-15 16:28:52 +0800

    更新 gogor api

--- src/gogoro/List.js
+++ src/gogoro/List.js
@@ -1,47 +1,86 @@
 import React, { useState, useEffect } from 'react'
-import { Item, Input } from 'semantic-ui-react'
+import { map } from 'lodash'
+import { Item, Input, Select } from 'semantic-ui-react'
 import axios from 'axios'
+
+const cities = [
+	'台北市',
+	'基隆市',
+	'新北市',
+	'宜蘭縣',
+	'桃園市',
+	'新竹市',
+	'新竹縣',
+	'苗栗縣',
+	'台中市',
+	'彰化縣',
+	'南投縣',
+	'雲林縣',
+	'嘉義縣',
+	'嘉義市',
+	'台南市',
+	'高雄市',
+	'屏東縣',
+	'台東縣',
+	'花蓮縣',
+	'連江縣'
+]
+
+const cityOptions = map(cities, city => ({
+	key: city,
+	text: city,
+	value: city
+}))
 
 const StationList = () => {
 	const [station, setStation] = useState([])
 	const [search, setSearch] = useState('')
-	useEffect(() => {
-		axios
-			.get('https://wapi.gogoro.com/tw/api/vm/list')
-			.then(response => {
-				// handle success
-				const preparation = []
-				// 資料格式預處理
-				response.data.data.map((d, i) => {
-					const LocName = JSON.parse(d.LocName)
-					const Address = JSON.parse(d.Address)
-					const District = JSON.parse(d.District)
-					const City = JSON.parse(d.City)
-					return preparation.push({
-						Id: d.Id,
-						LocName: LocName,
-						Latitude: d.Latitude,
-						Longitude: d.Longitude,
-						ZipCode: d.ZipCode,
-						Address: Address,
-						District: District,
-						State: 1,
-						City: City,
-						AvailableTime: d.AvailableTime,
-						AvailableTimeByte: d.AvailableTimeByte
-					})
+	const [city, setCity] = useState(null)
+	const fetchAPI = axios
+		.get('https://wapi.gogoro.com/tw/api/vm/list')
+		.then(response => {
+			// handle success
+			const preparation = []
+			// 資料格式預處理
+			response.data.data.map((d, i) => {
+				const LocName = JSON.parse(d.LocName)
+				const Address = JSON.parse(d.Address)
+				const District = JSON.parse(d.District)
+				const City = JSON.parse(d.City)
+				return preparation.push({
+					Id: d.Id,
+					LocName: LocName,
+					Latitude: d.Latitude,
+					Longitude: d.Longitude,
+					ZipCode: d.ZipCode,
+					Address: Address,
+					District: District,
+					State: 1,
+					City: City,
+					AvailableTime: d.AvailableTime,
+					AvailableTimeByte: d.AvailableTimeByte
 				})
-				return setStation(preparation)
 			})
-			.catch(function(error) {
-				// handle error
-				console.log(error)
-			})
+			return setStation(preparation)
+		})
+		.catch(function(error) {
+			// handle error
+			console.log(error)
+		})
+	useEffect(() => {
+		return () => {
+			fetchAPI
+		}
 	}, [])
 
 	const filterList = () => {
 		let updatedList = station.filter(item => {
-			return item.Address.List[1].Value.indexOf(search) !== -1
+				console.log(city)
+			if (city) {
+				return item.District.List[1].Value === city
+			} else {
+				return item.Address.List[1].Value.indexOf(search) !== -1
+			}
 		})
 		let data = updatedList.map((item, index, array) => {
 			return (
@@ -59,6 +98,7 @@ const StationList = () => {
 	return (
 		<React.Fragment>
 			<Input value={search} onChange={e => setSearch(e.target.value)} />
+			<Select value={city} onChange={e => setCity(e.target.value)} placeholder="選擇縣市" options={cityOptions} />
 			<Item.Group>{filterList()}</Item.Group>
 			<pre>{JSON.stringify(station, null, 2)}</pre>
 		</React.Fragment>
