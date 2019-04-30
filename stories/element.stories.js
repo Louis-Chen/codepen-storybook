419be89806675dd6e042d8f2aa2d8fda2e3a32c0
LuisChen <leo5916267@gmail.com>
2019-04-30 15:12:16 +0800

    功能 測試

--- stories/element.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import { storiesOf } from '@storybook/react'
-import { Button } from '../src/ui/Elements/Button'
-
-storiesOf('Elements/按鈕', module)
-	.add('Primary', () => <Button.Primary>Primary Button</Button.Primary>)
-	.add('Default', () => <Button.Default>Default Button</Button.Default>)
